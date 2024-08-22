7b7d9b2017ee95fea583438055e9ae8594078cfa
Agustin Greco <agustin.greco.pastorino@gmail.com>
2024-08-22 13:05:58 -0300

    Comented unused imports

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,21 +1,22 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+// import Image from "next/image";
+// import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+// import InvoiceStatus from "@/app/ui/invoices/status";
+// import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+// import { fetchFilteredInvoices } from "@/app/lib/data";
 
-export default async function InvoicesTable({
-  query,
-  currentPage,
-}: {
-  query: string;
-  currentPage: number;
-}) {
-  const invoices = await fetchFilteredInvoices(query, currentPage);
+// export default async function InvoicesTable({
+//   query,
+//   currentPage,
+// }: {
+//   query: string;
+//   currentPage: number;
+// }) {
+// const invoices = await fetchFilteredInvoices(query, currentPage);
 
+export default async function InvoicesTable() {
   return (
     <div className="mt-6 flow-root">
-      <div className="inline-block min-w-full align-middle">
+      {/* <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
             {invoices?.map((invoice) => (
@@ -118,7 +119,8 @@ export default async function InvoicesTable({
             </tbody>
           </table>
         </div>
-      </div>
+      </div> */}
+      todo invoices table
     </div>
   );
 }
